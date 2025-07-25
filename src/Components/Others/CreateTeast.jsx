b6ceedd55f47350275e1b974231bb8b096757566
kapilsingh09 <karan404katyura@gamil.com>
2025-07-25 22:08:55 +0530

    create task update

--- src/Components/Others/CreateTeast.jsx
+++ src/Components/Others/CreateTeast.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
 
 const CreateTask = () => {
+  // const [first, setfirst] = useState(second)
+  const [task_title, setTask_title] = useState('')
+  const [task_description, setTask_description] = useState('')
+  const [assignto, setAssignto] = useState('')
+  const [category, setCategory] = useState('')
+  const [task_date, setTask_date] = useState('')
+
+  const [Task, SetTask] = useState([])
+  const submitHandler = (e) =>{
+    e.prevetDefault()
+    console.log("task created");
+    setAssignto('')
+    setCategory('')
+    setTask_date('')
+    setTask_description('')
+    setTask_title('')
+
+    SetTask()
+    
+  }
   return (
     <div className="p-10 bg-zinc-900 min-h-screen text-white">
       <h2 className="text-3xl font-semibold mb-6 text-center">Create New Task</h2>
       
-      <form className="flex flex-wrap bg-[#1C1C1C] p-8 rounded-xl shadow-lg gap-10">
+      <form
+      onClick={(e)=>{
+        submitHandler(e)
+      }}
+      className="flex flex-wrap bg-[#1C1C1C] p-8 rounded-xl shadow-lg gap-10">
         {/* Left Side */}
         <div className="flex-1 space-y-6">
           <div>
             <label className="block text-sm mb-2 font-medium">Task Title</label>
             <input
+            value={task_title}
+            onChange={(e)=>{setTask_title(e.target.value)}}
               type="text"
               placeholder="Make a UI design"
               className="w-full px-4 py-2 rounded-md bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-2 focus:ring-green-400"
@@ -20,6 +46,8 @@ const CreateTask = () => {
           <div>
             <label className="block text-sm mb-2 font-medium">Assign To</label>
             <input
+              value={assignto}
+              onChange={(e)=>{setAssignto(e.target.value)}}
               type="text"
               placeholder="Employee name"
               className="w-full px-4 py-2 rounded-md bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-2 focus:ring-green-400"
@@ -29,6 +57,8 @@ const CreateTask = () => {
           <div>
             <label className="block text-sm mb-2 font-medium">Category</label>
             <input
+            value={category}
+            onChange={(e)=>{setCategory(e.target.value)}}
               type="text"
               placeholder="Design, Development, etc."
               className="w-full px-4 py-2 rounded-md bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-2 focus:ring-green-400"
@@ -39,6 +69,8 @@ const CreateTask = () => {
             <label className="block text-sm mb-2 font-medium">Deadline</label>
             <input
               type="date"
+              value={task_date}
+              onChange={(e)=>{setTask_date(e.target.value)}}
               className="w-full px-4 py-2 rounded-md bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-2 focus:ring-green-400"
             />
           </div>
@@ -49,6 +81,8 @@ const CreateTask = () => {
           <div>
             <label className="block text-sm mb-2 font-medium">Description</label>
             <textarea
+            value={task_description}
+            onChange={(e)=>{setTask_description(e.target.value)}}
               rows="8"
               placeholder="Write task details..."
               className="w-full px-4 py-2 rounded-md bg-zinc-800 text-white border border-zinc-700 focus:outline-none focus:ring-2 focus:ring-green-400 resize-none"
